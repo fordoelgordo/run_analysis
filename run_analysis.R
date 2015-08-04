269e4f842ead2821fe9dde347783b65d0be9f837
fordoelgordo <fst.john@carolinacat.com>
2015-08-04 11:30:59 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 run_analysis <- function() {
-# read various data sets into R that were contained in the .zip file
+# Read various data sets into R that were contained in the .zip file
+# The reading of the data assumes that your working directory is set properly so that the data from the .zip file can be
+# read in from wherever you have the files stored
   X_train <- read.table("./data/UCI HAR Dataset/train/x_train.txt")
   Y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
   subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
@@ -9,12 +11,13 @@ run_analysis <- function() {
   activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
   features <- read.table("./data/UCI HAR Dataset/features.txt")
   
-  
 # combine the test and train data sets by subject ID and activity ID for each observation
   train <- cbind(subject_train,Y_train,X_train)
   test <- cbind(subject_test,Y_test,X_test)
+  
 # merge the train and test data sets together
   combinedata <- rbind(train,test)
+  
 # provide logical names for the columns in the combined dataset
   colnames(combinedata)[1:2] <- c("Subject_ID","Activity_ID")
   cfeatures <- as.character(features[,2])
@@ -26,6 +29,7 @@ run_analysis <- function() {
   i <- grep("mean",colnames(combinedata))
   j <- grep("std",colnames(combinedata))
   subcombine <- combinedata[,c(1,2,i,j)]
+  
 # give the activity IDs in the dataset descriptive names
   vlabels <- as.vector(as.character(activity_labels[,2]))
   subcombine <- mutate(subcombine, Activity_desc = factor(subcombine$Activity_ID,labels = vlabels))
